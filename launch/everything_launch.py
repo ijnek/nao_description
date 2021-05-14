71afa93049cde26dd6850864e51004ae6afd3313
Kenji Brameld <kenjibrameld@gmail.com>
2021-05-14 17:03:14 +0900

    pass all tests

--- launch/everything_launch.py
+++ launch/everything_launch.py
@@ -1,9 +1,27 @@
+# Copyright 2021 Kenji Brameld
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
+
 from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
     return LaunchDescription([
@@ -13,7 +31,9 @@ def generate_launch_description():
             package='rviz2',
             executable='rviz2',
             arguments=[
-                "-d", os.path.join(get_package_share_directory('nao_description'), 'rviz', 'nao.rviz')
+                '-d', os.path.join(
+                    get_package_share_directory('nao_description'),
+                    'rviz', 'nao.rviz')
             ]
         ),
         Node(
@@ -28,6 +48,7 @@ def declare_launch_argument():
             'use_sim_time',
             default_value='false',
             description='Use simulation (Gazebo) clock if true')
+
 
 def robot_state_publisher():
 
